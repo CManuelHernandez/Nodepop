4b7a13e1fe0e9620386fc8630d3825fff9353ffb
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-02-05 10:51:48 +0100

    Elimno usuarios y reajuste apps

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var ads = require('./routes/ads');
-
 var app = express();
 
 // view engine setup
@@ -20,9 +16,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/ads', ads);
+app.use('/', require('./routes/index'));
+app.use('/ads', require('./routes/ads'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
